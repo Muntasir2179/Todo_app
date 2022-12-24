b785e08d90c84c0e957b1d37e71304fe6ffa195f
Muntaisr Ahmed <muntasirahmed2179@gmail.com>
2022-12-24 18:59:14 +0600

    Login Added

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login as loginUser
 
 # Create your views here.
 
@@ -9,7 +10,27 @@ def home(request):
 
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'GET':
+        form = AuthenticationForm()
+        context = {
+            "form": form
+        }
+        return render(request, 'login.html', context=context)
+
+    else:
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                loginUser(request, user)
+                return redirect('home_page')
+        else:
+            context = {
+                "form": form
+            }
+            return render(request, 'login.html', context=context)
 
 
 def signup(request):
@@ -28,6 +49,6 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             if user is not None:
-                return redirect('home_page')
+                return redirect('login_page')
         else:
             return render(request, 'signup.html', context=context)
